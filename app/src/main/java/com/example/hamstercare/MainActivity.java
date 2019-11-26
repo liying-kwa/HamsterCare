595c30fead9f5b70ae20e7b78f851fe9f5923f36
Li Ying Kwa <li5ying@hotmail.com>
2019-11-27 00:18:49 +0800

    Put up menu toolbar with about and help in it

--- app/src/main/java/com/example/hamstercare/MainActivity.java
+++ app/src/main/java/com/example/hamstercare/MainActivity.java
@@ -2,9 +2,8 @@ package com.example.hamstercare;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -22,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         // Set up bottom navigation bar
         bottomNavigationBar = getSupportActionBar();
@@ -52,14 +50,6 @@ public class MainActivity extends AppCompatActivity {
                     bottomNavigationBar.setTitle("Settings");
                     loadFragment(new SettingsFragment());
                     return true;
-                case R.id.help:
-                    bottomNavigationBar.setTitle("Help");
-                    loadFragment(new HelpFragment());
-                    return true;
-                case R.id.about:
-                    bottomNavigationBar.setTitle("About Us");
-                    loadFragment(new AboutFragment());
-                    return true;
             }
             return false;
         }
@@ -70,6 +60,34 @@ public class MainActivity extends AppCompatActivity {
         transaction.replace(R.id.container, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.help) {
+            Intent intent = new Intent(this, HelpActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        if (id == R.id.about) {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
